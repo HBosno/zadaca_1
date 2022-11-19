4a5707bae219a06eb0d3f74901a2fb32593be11c
HBosno <hbosno1@etf.unsa.ba>
2022-11-19 19:46:41 +0100

    App class for parsing progress 2 - comments added

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -6,7 +6,13 @@ package ba.unsa.etf.rpr;
  *
  */
 public class App{
-
+    /**
+     * Main method that will handle console input, construct a string to pass to Dijkstra's algorithm and return the
+       value of the arithmetic expression contained in the string
+     * @param args - Input from console, array of strings to be parsed, validated and merged into a single string
+     *               for ExpressionEvaluator to work with next
+     * @throws RuntimeException - Will throw an exception in case of an invalid arithmetic expression
+     */
     public static void main( String[] args ) throws RuntimeException {
         try {
             StringBuilder builder = new StringBuilder();
@@ -14,6 +20,7 @@ public class App{
                 throw new RuntimeException("Izraz nije aritmetički validan!");
             }
             for (String s : args) {
+
                 builder.append(s).append(" ");
             }
         }
