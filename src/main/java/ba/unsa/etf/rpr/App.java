99e87019b55c4b66b8a5a9b963db3f92059a7394
HBosno <hbosno1@etf.unsa.ba>
2022-11-19 22:57:49 +0100

    App class for parsing progress 6 - major changes, App class now supports sending the expression as string "expression", everything in App class modified to work on char level instead of String as in version 5.

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,6 +1,8 @@
 package ba.unsa.etf.rpr;
 
-import static org.apache.maven.shared.utils.StringUtils.isNumeric;
+import static java.lang.Character.isDigit;
+
+//import static org.apache.maven.shared.utils.StringUtils.isNumeric;
 
 /**
  * Main class for parsing console input using args function parameter and validating said input, before passing it to
@@ -16,55 +18,57 @@ public class App{
      * @throws RuntimeException - Will throw an exception in case of an invalid arithmetic expression
      */
     public static void main( String[] args ) throws RuntimeException {
-        try {
-            StringBuilder builder = new StringBuilder();
-            if (!args[0].equals("(") || !args[args.length - 1].equals(")")) {
-                throw new RuntimeException("Izraz nije aritmetički validan!");
-            }
-            int leftParentheses = 0;
-            int rightParentheses = 0;
-            int numberOfOperators = 0;
-            for (int i = 0; i < args.length; i++) {  // replaced the ranged for loop as we will probably be in need to access next and previous String relative to current position at one point
-                if(!isNumeric(args[i]) && !args[i].equals("(") && !args[i].equals(")") && !checkIfOperator(args[i])){
+            try {
+                String s = args[0];
+                if (!(s.charAt(0) == '(') || !(s.charAt(s.length()-1) == ')')) {
                     throw new RuntimeException("Izraz nije aritmetički validan!");
                 }
-                if(args[i].equals("(")){
-                    leftParentheses = leftParentheses + 1;
-                }
-                else if(args[i].equals(")")){
-                    rightParentheses = rightParentheses + 1;
-                }
-                else if(checkIfOperator(args[i])){
-                    checkOperatorSurroundings(args, i);
-                    numberOfOperators = numberOfOperators + 1;
-                }
-                builder.append(args[i]);
-                if(i != args.length - 1){
-                    builder.append(" ");
-                }
-            }
+                int leftParentheses = 0;
+                int rightParentheses = 0;
+                int numberOfOperators = 0;
+                for (int i = 0; i < s.length(); i++) {  // replaced the ranged for loop as we will probably be in need to access next and previous String relative to current position at one point
+                    if(i%2 != 0 && i != s.length() - 1 && s.charAt(i) != ' '){ // whitespaces are positioned on odd index numbers, except for eventually the last position, which should always be ')'
+                        throw new RuntimeException("Izraz nije aritmetički validan!");
+                    }
+                    if (!isDigit(s.charAt(i)) && s.charAt(i)!='(' && s.charAt(i)!=')' && !checkIfOperator(s.charAt(i), i, s)) {
+                        throw new RuntimeException("Izraz nije aritmetički validan!");
+                    }
+                    if (s.charAt(i) == '(') {
+                        leftParentheses = leftParentheses + 1;
+                    } else if (s.charAt(i) == ')') {
+                        rightParentheses = rightParentheses + 1;
+                    } else if (checkIfOperator(s.charAt(i), i, s)) {
+                        checkOperatorSurroundings(s, i);
+                        numberOfOperators = numberOfOperators + 1;
+                    }
 
-            // should we analyze the expressions we are working with, we will come to the conclusion that each left parenthesis corresponds to a single operator
-            // obviously, we also need a matching number of right parentheses with respect to their left counterparts
-            if(leftParentheses != numberOfOperators || leftParentheses != rightParentheses){
-                throw new RuntimeException("Izraz nije aritmetički validan!");
+                }
+
+                // should we analyze the expressions we are working with, we will come to the conclusion that each left parenthesis corresponds to a single operator
+                // obviously, we also need a matching number of right parentheses with respect to their left counterparts
+                if (leftParentheses != numberOfOperators || leftParentheses != rightParentheses) {
+                    throw new RuntimeException("Izraz nije aritmetički validan!");
+                }
+                ExpressionEvaluator evaluator = new ExpressionEvaluator();
+                System.out.println("Vrijednost unesenog aritmetičkog izraza: " + evaluator.evaluate(s));
+            } catch (RuntimeException e) {
+                e.printStackTrace();
             }
-            ExpressionEvaluator evaluator = new ExpressionEvaluator();
-            System.out.println("Vrijednost unesenog aritmetičkog izraza: " + evaluator.evaluate(builder.toString()));
         }
-        catch(RuntimeException e) {
-            e.printStackTrace();
-        }
-    }
 
     /**
-     * Checks if parameter String is one of the operators we are working with
+     * Checks if char parameter is one of the operators we are working with
      * Used in main method for better code readability
-     * @param s - String to be tested
-     * @return - returns true if parameter string is an operator, otherwise return value is false
+     * @param c - char to be tested
+     * @param i - index of the current char
+     * @param s - string containing the whole arithmetic expression
+     * @return - returns true if parameter char is an operator, otherwise return value is false
      */
-    private static boolean checkIfOperator(String s){
-        return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") || s.equals("sqrt");
+    private static boolean checkIfOperator(char c, int i, String s){
+        if(c == 's' && s.charAt(i+1) == 'q' && s.charAt(i+2) == 'r' && s.charAt(i+3) == 't'){
+            return true;
+        }
+        return c == '+' || c == '-' || c == '*' || c == '/';
     }
 
     /**
@@ -78,18 +82,18 @@ public class App{
      2. operator sqrt (, where operator is any operator excluding sqrt
      * Note that we can still have expressions such as a fourth root (number^(1/4) or sqrt(sqrt number)) however this is covered by case 1 as the valid expression would look as following:
        sqrt ( sqrt ( number ) )
-     * @param args - String array, main input from console
-     * @param i - index of the String we are currently testing
+     * @param s - string containing the whole arithmetic expression
+     * @param i - index of the char we are currently testing
      * @throws RuntimeException if the expression is not valid
      */
-    private static void checkOperatorSurroundings(String[] args, int i) throws RuntimeException{
-        if(!args[i].equals("sqrt")){
-            if(!(args[i-1].equals(")") && args[i+1].equals("(") || isNumeric(args[i-1]) && isNumeric(args[i+1]) || args[i-1].equals(")") && isNumeric(args[i+1]) || isNumeric(args[i-1]) && args[i+1].equals("("))){
+    private static void checkOperatorSurroundings(String s, int i) throws RuntimeException{
+        if(s.charAt(i) != 's'){
+            if(!(s.charAt(i-2) == ')' && s.charAt(i+2) == '('  || isDigit(s.charAt(i-2)) && isDigit(s.charAt(i+2))|| s.charAt(i-2) == ')' && isDigit(s.charAt(i+2)) || isDigit(s.charAt(i-2)) && s.charAt(i+2) == '(')){
                 throw new RuntimeException("Izraz nije aritmetički validan!");
             }
         }
         else{
-            if(!(args[i-1].equals("(") && args[i+1].equals("(") || checkIfOperator(args[i-1]) && !args[i-1].equals("sqrt") && args[i+1].equals("("))){
+            if(!(s.charAt(i-2) == '(' && s.charAt(i+2) == '(' || checkIfOperator(s.charAt(i-2), i-2, s) && s.charAt(i+2) == '(')){
                 throw new RuntimeException("Izraz nije aritmetički validan!");
             }
         }
