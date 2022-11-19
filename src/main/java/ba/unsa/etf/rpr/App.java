89197cc79586863abb75dee24ee8f51241a979f5
HBosno <hbosno1@etf.unsa.ba>
2022-11-19 19:40:13 +0100

    App class for parsing progress 1

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -2,12 +2,24 @@ package ba.unsa.etf.rpr;
 
 /**
  * Main class for parsing console input using args function parameter and validating said input, before passing it to
-   ExpressionEvaluator function.
+   ExpressionEvaluator function
  *
  */
-public class App 
-{
-    public static void main( String[] args ){
+public class App{
 
+    public static void main( String[] args ) throws RuntimeException {
+        try {
+            StringBuilder builder = new StringBuilder();
+            if (!args[0].equals("(") || !args[args.length - 1].equals(")")) {
+                throw new RuntimeException("Izraz nije aritmetički validan!");
+            }
+            for (String s : args) {
+                builder.append(s).append(" ");
+            }
+        }
+        catch(RuntimeException e) {
+            e.printStackTrace();
+        }
     }
 }
+
